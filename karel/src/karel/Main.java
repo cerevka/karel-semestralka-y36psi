e267d73259124592a76c5eec0a0713b42e2fad4f
honza@cermakjn.info <honza@cermakjn.info@e2a7a086-b9af-11de-a079-5d186ce21964>
2009-10-17 18:48:05 +0000

    pridano generovani jmen - naplnit jmenama kontrola jmena - opravit to equals

--- karel/src/karel/Main.java
+++ karel/src/karel/Main.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package karel;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +12,71 @@ package karel;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("test");
+    static int pokusJmeno = 0; //pokusy na jmeno
+    public String jmeno = "";
+
+    public static String vygenerujJmeno() {
+        String jmenoGen = "";
+        double cisloD = (10 * Math.random());
+        int cislo = (int) cisloD;
+        if (cislo == 0) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 1) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 2) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 3) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 4) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 5) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 6) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 7) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 8) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 9) {
+            jmenoGen = "blb";
+        }
+        if (cislo == 10) {
+            jmenoGen = "blb";
+        }
+        return jmenoGen;
     }
 
+    public static void kontrolaJmena(String text) {
+        String jmenoTest = "";
+        if (text.length()==0) {
+            System.exit(0);
+        }
+        for(int i=0; i<text.length(); i++) {
+             if (text.charAt(0) == ' ') {
+                 if (jmenoTest.equals(jmeno)==false) {
+                     System.exit(0);
+                 }
+             }
+             jmenoTest += text.charAt(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        jmeno = vygenerujJmeno();
+        System.out.println("220 Oslovuj mne " + jmeno);
+
+        Scanner vstup = new Scanner(System.in);
+        String text = vstup.nextLine();
+
+        kontrolaJmena(text);
+    }
 }
