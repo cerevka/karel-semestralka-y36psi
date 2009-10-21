60541c9b616345ff415f82c0554bfcf7ae983b98
honza@cermakjn.info <honza@cermakjn.info@e2a7a086-b9af-11de-a079-5d186ce21964>
2009-10-21 11:36:17 +0000

    dodelano hafo toho.. ted zbejva jen vychytat "opravit" a pokud vyjede ze sachovnice

--- karel/src/karel/Main.java
+++ karel/src/karel/Main.java
@@ -23,8 +23,8 @@ public class Main {
     public static String zacniHru() {
         String jmenoGen = "";
         double cisloD = (10 * Math.random());
-        poziceX = rand.nextInt() % 16; // rozsah -16 až +16
-        poziceY = rand.nextInt() % 16; // rozsah -16 až +16
+        poziceX = rand.nextInt() % 17; // rozsah -17 až +17
+        poziceY = rand.nextInt() % 17; // rozsah -17 až +17
         pokus = (Math.abs(rand.nextInt() % 12) + 1); //rozsah 1 - 13
         smer = (Math.abs(rand.nextInt() % 3) + 1); //rozsah 1 - 4
 
@@ -73,25 +73,24 @@ public class Main {
             System.exit(0);
         }
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ') {
+            if (text.charAt(i) == ' ') { //narazime na mezeru
                 if (jmenoTest.equals(jmeno) == false) {
-                    jmenoTest += text.charAt(i);
+                    jmenoTest += text.charAt(i); //jmeno neopodvida, pokracuj
                     i++;
                 } else {
                     //pokud jmeno odpovida
                     String vratka = "";
                     for (int p = (i + 1); p < text.length(); p++) {
-                        vratka += text.charAt(p);
+                        vratka += text.charAt(p); //vrat zbytek
                     }
                     return vratka;
                 }
             }
             jmenoTest += text.charAt(i);
-            if (i == text.length() - 1) { //pokud je i mensi jak delka
-                System.exit(0);
+            if (i == text.length() - 1) { //pokud je i mensi jak delka;
             }
         }
-        System.exit(0);
+        System.out.println("Chybne jmeno");
         return nic;
     }
 
@@ -103,94 +102,107 @@ public class Main {
     }
 
     public static boolean postupuj(String text, int chyba) {
+        int i = 0;
+        if (text.length() == 0) {//kotrola zda predchotzi metoda zamitla kontrolu jmena
+            pokus--;
+            return false;
+        }
         //generovani poruchy
-        if ((Math.abs(rand.nextInt() % 3) + 1) == 4) { //generovani 1-4, kazdy ctvrty je porouchany
+        if ((Math.abs(rand.nextInt() % 3) + 1) == 3) { //generovani 1-4, kazdy ctvrty je porouchany
             porucha = (Math.abs(rand.nextInt() % 8) + 1); //generovani bloku poruchy
+            System.out.println("570 PORUCHA BLOK " + porucha);
+            return false;
         }
-        //konec generovani
+        //konec generovani poruchy
         String postup = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ') {
-                if ((postup.equals("KROK") == true) || (postup.equals("VLEVO") == true) || (postup.equals("ZVEDNI") == true)) {
-                    jePorucha(); //kotrola jestli je porucha
-                    i = text.length(); //toto ukonci zapisovani
-
-                    // KROK
-                    if ((postup.equals("KROK") == true) && (text.length() == 4)) {
-                        if (smer == 1) {
-                            poziceX++;
-                        } else {
-                            if (smer == 2) {
-                                poziceY++;
-                            } else {
-                                if (smer == 3) {
-                                    poziceX--;
-                                } else {
-                                    if (smer == 4) {
-                                        poziceY--;
-                                    } else {
-                                        System.out.println("Nastala neznámá chyba");
-                                    }
-                                }
-                            }
-
-                        }
-                        return false;
-                    }
-                    //konec KROK
-                    //VLEVO
-                    if ((postup.equals("VLEVO") == true) && (text.length() == 5)) {
-                        if (smer == 4) {
-                            smer = 1;
-                        } else {
-                            smer++;
-                        }
-                        return false;
-                    }
-                    //konec VLEVO
-                    //ZVEDNI
-                    if ((postup.equals("ZVEDNI") == true) && (text.length() == 6) && (poziceX == 0) && (poziceY == 0)) {
-                        System.out.println("221 USPECH Text tajemství");
-                        return true;
-                    } else {
-                        if (postup.equals("ZVEDNI") == true) {
-                            System.out.println("550 NELZE ZVEDNOUT ZNACKU");
-                            pokus--;
-                            return false;
-                        }
-
-                    }
-                    //konec ZVEDNI
-                    //OPRAVIT
-                } else {
-                    if (postup.equals("OPRAVIT") == true) {
-                        int vratka = 0;
-                        for (int p = (i + 1); p < text.length(); p++) {
-                            vratka += text.charAt(p);
-                        }
-                        if (porucha == 0) { // je nejka porucha vubec?
-                            System.out.println("571 NENI PORUCHA");
-                            pokus--;
-                        } else {
-                            if (vratka != porucha) { //nesouhlasi porouchanj blok
-                                System.out.println("571 NENI PORUCHA");
-                                pokus--;
-                            } else {
-                                if (vratka == porucha) { //opraveni poruchy
-                                    porucha = 0;
-                                }
-                            }
-                        }
-                    } else {
-                        System.out.println("500 NEZNAMY PRIKAZ");
-                        pokus--;
-                    }
-                    //konec OPRAVIT
-                }
-                postup += text.charAt(i);
-            }
+        for (i = 0; i < text.length(); i++) { //jede cyklus
+            postup += text.charAt(i);
         }
+        if ((text.equals("KROK") == true) || (text.equals("VLEVO") == true) || (text.equals("ZVEDNI") == true)) { //pokud na ni narazi, zkontroluje jetsli text neni
+            jePorucha(); //kotrola jestli je porucha
+            // KROK
+            if ((text.equals("KROK") == true) && (text.length() == 4)) {
+                if (smer == 1) {
+                    poziceX++;
+                } else {
+                    if (smer == 2) {
+                        poziceY++;
+                    } else {
+                        if (smer == 3) {
+                            poziceX--;
+                        } else {
+                            if (smer == 4) {
+                                poziceY--;
+                            } else {
+                                System.out.println("Nastala neznámá chyba");
+                            }
+                        }
+                    }
+                }
+                vypisPolohu();
+                return false;
+            }
+            //konec KROK
+            //VLEVO
+            if ((text.equals("VLEVO") == true) && (text.length() == 5)) {
+                if (smer == 4) {
+                    smer = 1;
+                } else {
+                    smer++;
+                }
+                vypisPolohu();
+                return false;
+            }
+            //konec VLEVO
+            //ZVEDNI
+            if ((text.equals("ZVEDNI") == true) && (text.length() == 6) && (poziceX == 0) && (poziceY == 0)) {
+                System.out.println("221 USPECH Text tajemství");
+                return true;
+            } else {
+                if (text.equals("ZVEDNI") == true) {
+                    System.out.println("550 NELZE ZVEDNOUT ZNACKU");
+                    ukonciSpojeni();
+                }
+
+            }
+            //konec ZVEDNI
+            //OPRAVIT
+        } else {
+            if (text.equals("OPRAVIT") == true) {
+                int vratka = 0;
+                for (int p = (i + 1); p < text.length(); p++) {
+                    vratka += text.charAt(p);
+                }
+                if (porucha == 0) { // je nejka porucha vubec?
+                    System.out.println("571 NENI PORUCHA");
+                    ukonciSpojeni();
+                } else {
+                    if (vratka != porucha) { //nesouhlasi porouchanj blok
+                        System.out.println("571 NENI PORUCHA");
+                        ukonciSpojeni();
+                    } else {
+                        if (vratka == porucha) { //opraveni poruchy
+                            porucha = 0;
+                        }
+                    }
+                }
+            } else {
+                System.out.println("500 NEZNAMY PRIKAZ");
+                pokus--;
+            }
+            //konec OPRAVIT
+        }
+
         return false;
+    }
+
+    public static void ukonciSpojeni() {
+        System.out.println("Spojeni je ukonceno");
+        System.exit(0);
+    }
+
+    public static void vypisPolohu() {
+        System.out.println("250 OK (" + poziceX + "," + poziceY + ")");
     }
 
     public static void main(String[] args) {
@@ -205,11 +217,6 @@ public class Main {
 
             String postup = kontrolaJmena(text); //zkontroluje jmeno
             vyhra = postupuj(postup, chyba); //urci vyhra true/flase
-
-            if (pokus <= 0) {  //zkotroluje pocet pokusu
-                System.out.println("Překročen počet pokusů.s Ukončuji spojení.");
-                System.exit(0);
-            }
 
         }
     }
