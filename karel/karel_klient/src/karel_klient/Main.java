24e254df526fcc7cd047aa819bfddece7f6cec97
honza@cermakjn.info <honza@cermakjn.info@e2a7a086-b9af-11de-a079-5d186ce21964>
2009-10-21 13:28:53 +0000

    test

---
+++ karel/karel_klient/src/karel_klient/Main.java
@@ -0,0 +1,16 @@
+package karel_klient;
+
+/**
+ * @version 1.0
+ * @author Jan Cermak (cermaja9@fel.cvut.cz) & Tomas Cerevka (cerevtom@fel.cvut.cz)
+ * Semestrální práce z Y36PSI - práce číslo 1 - Karel
+ * zadání: https://dsn.felk.cvut.cz/wiki/vyuka/y36psi/cviceni/uloha1-karel-zadani
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
